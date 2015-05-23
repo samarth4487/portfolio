2adaa581438e81baf9dd77a0a213edc76bd876cc
Samarth Paboowal <samarthpaboowal@yahoo.co.in>
2015-05-23 14:33:14 +0530

    home page redefined

--- js/helper.js
+++
@@ -1,5 +0,0 @@
-var htmlskill='<div class="col-md-2 skill-name"><svg height="200" width="200"><circle cx="100" cy="100" r="80" stroke="white" stroke-width="3" fill="#C3834D" /></svg></div>';
-var skills = ['C','C++','HTML5','CSS3','Javascript','jQuery','AngularJs','node.js','Python','Java','Swift','Ruby'];
-for(skill in skills){
-	$('.skill').append(htmlskill);
-}
